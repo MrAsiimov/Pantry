84851346002d7230a69790bcf785c2688683f5b6
Luca <lucas98.rizzo@gmail.com>
2025-08-18 16:19:57 +0200

    Added rounded button style

--- app/src/main/java/com/luca/pantry/MainActivity.kt
+++ app/src/main/java/com/luca/pantry/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.luca.pantry
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat.getDrawable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luca.pantry.R.id.imageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        val shape: Drawable? = getDrawable(resources, R.drawable.rounded_menu_button, getTheme())
+        val menu_btn: ImageButton = findViewById(imageButton)
+        menu_btn.background = shape
+
     }
 }
