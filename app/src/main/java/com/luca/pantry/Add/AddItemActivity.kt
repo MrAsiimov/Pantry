9ea7062f5ef1783240b2454876ca1d1ddb4123c4
Luca <lucas98.rizzo@gmail.com>
2025-09-05 20:54:52 +0200

    Created AddItemActivity.kt and AddContainerActivity.kt. Added startActivity method on relative popup menu item

--- app/src/main/java/com/luca/pantry/Add/AddItemActivity.kt
+++ app/src/main/java/com/luca/pantry/Add/AddItemActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.luca.pantry.Add.ui.theme.PantryTheme
+import com.luca.pantry.ui.theme.PantryTheme
 import com.luca.pantry.BaseActivity
 import com.luca.pantry.R
 
